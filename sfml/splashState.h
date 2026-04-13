754ff2f2121b3905a3ecef4ea7d24ea4f7c967f1
noleysc <152906259+noleysc@users.noreply.github.com>
2026-04-13 16:22:55 -0400

    Delete sfml directory

--- sfml/splashState.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-#include "windowDef.h"
-#include "game.h"
-class splashState::public game
-{
-};
-
--- sfml/windowDef.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#define SCREEN_WIDTH 960
-#define SCREEN_HEIGHT 1280
-#define SPLASH_STATE_SHOW_TIME 4.0
-#define SPLASH_SCENE_BACKGROUND_FILEPATH "tex/splashState.png"
